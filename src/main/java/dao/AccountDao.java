db8ef957068e3cc39b919ecc5d45b24f6069225f
Denisa <anghelescu.denisa@yahoo.com>
2021-11-02 18:31:12 +0200

    banking app

---
+++ src/main/java/dao/AccountDao.java
@@ -0,0 +1,28 @@
+package dao;
+
+import model.Account;
+import model.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class AccountDao extends GenericDao<Account> {
+    public Account findByIban(String iban) {
+
+        Session session = sessionFactory.openSession();
+
+        List<Account> result = session.createQuery("FROM Account a WHERE a.iban = :iban")
+                .setParameter("iban", iban)
+                .list();
+        System.out.println(result);
+        session.close();
+
+        if(result.size()==0){
+            return null;
+        }
+        else return result.get(0) ;
+    }
+
+
+}
